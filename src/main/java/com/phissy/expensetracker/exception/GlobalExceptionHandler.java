87b6a0991d420d7dc7efede2cddc2d8fa5f3fe92
aolufisayo <phissycodes@hotmail.com>
2023-06-12 19:26:23 +0100

    handled general exception

--- src/main/java/com/phissy/expensetracker/exception/GlobalExceptionHandler.java
+++ src/main/java/com/phissy/expensetracker/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
@@ -21,5 +22,23 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         errorObject.setTimeStamp(new Date());
         return new ResponseEntity<ErrorObject>(errorObject, HttpStatus.NOT_FOUND);
 
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorObject> handleException(MethodArgumentTypeMismatchException exception, WebRequest request){
+        ErrorObject errorObject = new ErrorObject();
+        errorObject.setStatusCode(HttpStatus.BAD_REQUEST.value());
+        errorObject.setMessage(exception.getMessage());
+        errorObject.setTimeStamp(new Date());
+        return new ResponseEntity<ErrorObject>(errorObject, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorObject> handleGeneralException(Exception exception, WebRequest request){
+        ErrorObject errorObject = new ErrorObject();
+        errorObject.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        errorObject.setMessage(exception.getMessage());
+        errorObject.setTimeStamp(new Date());
+        return new ResponseEntity<ErrorObject>(errorObject, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
